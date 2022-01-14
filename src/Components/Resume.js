a3be17728b621425727283821d417be49fcd1c17
Juan Jose <juanjmm@gmail.com>
2022-01-13 19:41:25 -0600

    Carousel Picture

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -32,6 +32,18 @@ const Resume = ({ data }) => {
 
     return (
         <section id="resume">
+
+            <div className="row work">
+
+                <div className="three columns header-col">
+                    <h1><span>Work</span></h1>
+                </div>
+
+                <div className="nine columns main-col">
+                    {resume.work}
+                </div>
+            </div>
+
             <div className="row education">
                 <div className="three columns header-col">
                     <h1><span>Education</span></h1>
@@ -45,20 +57,6 @@ const Resume = ({ data }) => {
                     </div>
                 </div>
             </div>
-
-
-            <div className="row work">
-
-                <div className="three columns header-col">
-                    <h1><span>Work</span></h1>
-                </div>
-
-                <div className="nine columns main-col">
-                    {resume.work}
-                </div>
-            </div>
-
-
 
             <div className="row skill">
 
@@ -78,6 +76,9 @@ const Resume = ({ data }) => {
                     </div>
                 </div>
             </div>
+
+
+
         </section>
     )
 }
